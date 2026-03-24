ebfcf75f7ad7cfb677b597a5f84aa555e6d43aad
Thomas Iafrate <113009615+thomasiafrate1@users.noreply.github.com>
2026-03-24 13:56:50 +0100

    fix : 213

--- ymeal-quiz/src/app/page.tsx
+++ ymeal-quiz/src/app/page.tsx
@@ -233,7 +233,7 @@ useEffect(() => {
 
         <div className="relative bg-white/85  p-8">
           <p className="text-sm text-slate-500">Lieu</p>
-          <p className="font-black text-lg mt-1">Salle 205</p>
+          <p className="font-black text-lg mt-1">Salle 213</p>
         </div>
       </div>
 
